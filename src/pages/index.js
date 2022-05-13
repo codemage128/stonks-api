e79cb4c9cceca06c889f0332294466fa8216b619
Asilbek Normuminov <asilbeknormuminov522@gmail.com>
2022-05-13 10:02:15 -0400

    <a> to <Link>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,12 @@
 import Head from "next/head";
+import Link from "next/link";
 import Upcomming from "../pages/upcoming";
 
 export default function Home() {
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <a
-          href="/upcoming"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
-        >
-          Upcoming Events
-        </a>
+        <Link href="/about">Upcoming Events</Link>
       </div>
     </div>
   );
